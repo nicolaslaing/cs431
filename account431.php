61f29baacfa3446eb872ae644719e54fa9c97032
nicolaslaing <34254569+nicolaslaing@users.noreply.github.com>
2017-12-18 11:55:32 -0500

    Update account431.php

--- account431.php
+++ account431.php
@@ -1,12 +1,12 @@
 <?php
 // connection variables
 $user = "root";
-$password = "";
+$pass = "";
 $project = "nal9";
 $driver = "MySQL ODBC 5.3 ANSI Driver";
 $host = "localhost";
 
 
 $connection = "Driver=$driver; Server=$host; DATABASE=$project"; // connection string
-$conn = odbc_connect($connection, $user, $pass); // establish odbc connection
+$db = odbc_connect($connection, $user, $pass); // establish odbc connection
 ?>
